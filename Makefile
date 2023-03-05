3cc18a05749463b34241ce469e547b4730716287
Randolf Tjandra <randolftjandra@gmail.com>
2023-03-05 09:41:40 -0800

    Refactor key press handling
    Reviewer: none
    Issue: DEV-31

--- Makefile
+++ Makefile
@@ -10,6 +10,7 @@ build-wk:
 			 ./wk/model.go \
 			 ./wk/command.go \
 			 ./wk/mock_command.go \
+			 ./wk/navigation.go \
 			 ./wk/views.go
 
 package-wk: build-wk
--- wk/model.go
+++ wk/model.go
@@ -57,12 +57,27 @@ func initialModel(commander Commander, view PageView, subjectRepo db.SubjectRepo
 // Receives information and updates the model
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case errMsg:
+		m.err = msg
+		return m, tea.Quit
+
+	// key press
+	case tea.KeyMsg:
+		switch m.currentPage {
+		case IndexView:
+			return m.handleIndexKeyPress(msg)
+		case SummaryView:
+			return m.handleSummaryKeyPress(msg)
+		default:
+			return m.handleDefaultKeyPress(msg)
+		}
+
 	case *wanikaniapi.User:
 		m.User = msg
 		return m, nil
+
 	case *wanikaniapi.Summary:
 		m.Summary = msg
-		m.SummaryLessons = []*wanikaniapi.SummaryLesson{}
 		// prep summary lessons
 		for _, lesson := range m.Summary.Data.Lessons {
 			if len(lesson.SubjectIDs) == 0 {
@@ -71,7 +86,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.SummaryLessons = append(m.SummaryLessons, lesson)
 		}
 		// prep summary reviews
-		m.SummaryReviews = []*wanikaniapi.SummaryReview{}
 		for _, review := range m.Summary.Data.Reviews {
 			if len(review.SubjectIDs) == 0 {
 				continue
@@ -80,64 +94,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	case []*wanikaniapi.Subject:
 		m.SummarySubjects[m.cursors[SummaryView]] = msg
-	case errMsg:
-		m.err = msg
-		return m, tea.Quit
-	// key press
-	case tea.KeyMsg:
-		return m.handleKeyPress(msg)
 	}
 
-	return m, nil
-}
-
-func (m model) handleKeyPress(key tea.KeyMsg) (tea.Model, tea.Cmd) {
-	switch key.String() {
-	case "esc":
-		m.currentPage = IndexView
-		return m, nil
-	case "q":
-		return m, tea.Quit
-	case "up", "k":
-		if m.cursors[m.currentPage] > 0 {
-			m.cursors[m.currentPage]--
-		}
-	case "down", "j":
-		switch m.currentPage {
-		case IndexView:
-			if m.cursors[m.currentPage] < len(m.navChoices)-1 {
-				m.cursors[m.currentPage]++
-			}
-		case SummaryView:
-			if m.cursors[m.currentPage] < len(m.SummaryReviews)+len(m.SummaryLessons)-1 {
-				m.cursors[m.currentPage]++
-			}
-		}
-	case "enter":
-		switch m.currentPage {
-		case IndexView:
-			m.currentPage = m.navChoices[m.cursors[IndexView]]
-			// Set up what has to happen when a new page is selected
-			switch m.currentPage {
-			case SummaryView:
-				return m, m.commander.GetSummary
-			}
-		case SummaryView:
-			m.SummaryExpansion[m.cursors[SummaryView]] = !m.SummaryExpansion[m.cursors[SummaryView]]
-			if m.SummaryExpansion[m.cursors[SummaryView]] {
-				cursor := m.cursors[SummaryView]
-				var subjectIDs []wanikaniapi.WKID
-				if cursor < len(m.SummaryLessons) {
-					subjectIDs = m.SummaryLessons[cursor].SubjectIDs
-				} else {
-					cursor = cursor - len(m.SummaryLessons)
-					subjectIDs = m.SummaryReviews[cursor].SubjectIDs
-				}
-				return m, m.commander.GetSubjects(subjectIDs)
-			}
-		case AccountView:
-			return m, nil
-		}
-	}
 	return m, nil
 }
