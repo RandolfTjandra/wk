e5f5542f4aaf5db411870928219f39404f5e8c02
Randolf Tjandra <randolftjandra@gmail.com>
2023-03-28 16:26:49 -0700

    wip
    Reviewer: none
    Issue: none

--- pkg/levels/model.go
+++ pkg/levels/model.go
@@ -3,6 +3,7 @@ package levels
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/brandur/wanikaniapi"
 	"github.com/charmbracelet/bubbles/spinner"
@@ -61,12 +62,14 @@ func (m *model) View() string {
 	for _, level := range m.Levels {
 		var passedString string
 		pass := level.Data.PassedAt
+		var days int
 		if pass != nil {
-			passedString = pass.String()
+			passedString = pass.Format(time.DateOnly)
+			days = int(level.Data.PassedAt.Sub(*level.Data.StartedAt).Hours() / 24)
 		} else {
 			passedString = "in progress"
 		}
-		b.WriteString(fmt.Sprintf("%d: %s\n", level.Data.Level, passedString))
+		b.WriteString(fmt.Sprintf("%2d: %s %d days\n", level.Data.Level, passedString, days))
 	}
 
 	return b.String() + "\n"
